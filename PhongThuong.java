562c7da86bca9f77afc863cee4594c92ab8756c9
ZaKiet <kietchet234@gmail.com>
2025-10-14 11:04:21 +0700

    Update PhongThuong.java

--- PhongThuong.java
+++ PhongThuong.java
@@ -4,16 +4,13 @@ import java.util.Scanner;
 
 public class PhongThuong extends Phong {
     private double dichVuNT;
-    private double PhiDuaDon;
-
-    public PhongThuong() {
-    }
+    private double phiDuaDon;
 
     public PhongThuong(String maPhong, float donGia, int soNguoi, int soGiuong, Boolean tinhTrang, double dichVuNT,
-            double PhiDuaDon) {
+            double phiDuaDon) {
         super(maPhong, donGia, soNguoi, soGiuong, tinhTrang);
         this.dichVuNT = dichVuNT;
-        this.PhiDuaDon = PhiDuaDon;
+        this.phiDuaDon = phiDuaDon;
     }
 
     public double getDVNT() {
@@ -25,56 +22,31 @@ public class PhongThuong extends Phong {
     }
 
     public double getPhiDuaDon() {
-        return PhiDuaDon;
+        return phiDuaDon;
     }
 
-    public void setPhiDuaDon(double PhiDuaDon) {
-        this.PhiDuaDon = PhiDuaDon;
+    public void setPhiDuaDon(double phiDuaDon) {
+        this.phiDuaDon = phiDuaDon;
     }
 
-    @Override
     public String getLoaiPhong() {
-        return "Thường";
+        return "Thuong";
     }
 
-    @Override
     public void nhap() {
+        super.nhap();
         Scanner sc = new Scanner(System.in);
-        System.out.println("=== NHẬP THÔNG TIN PHÒNG THƯỜNG ===");
-        System.out.print("Nhập mã phòng: ");
-        this.maPhong = sc.nextLine();
-        System.out.print("Nhập đơn giá: ");
-        this.donGia = sc.nextFloat();
-        System.out.print("Nhập số người: ");
-        this.soNguoi = sc.nextInt();
-        System.out.print("Nhập số giường: ");
-        this.soGiuong = sc.nextInt();
-        System.out.print("Nhập tình trạng: ");
-        this.tinhTrang = sc.nextBoolean();
-        System.out.print("Nhập phí đưa đón: ");
-        this.PhiDuaDon = sc.nextDouble();
-        System.out.print("Nhập phí dịch vụ ngoài trời: ");
-        this.dichVuNT = sc.nextDouble();
+        System.out.println("=== NHAP THONG TIN PHONG THUONG ===");
+        System.out.print("Nhap phi dua don: ");
+        phiDuaDon = sc.nextDouble();
+        System.out.print("Nhap phi dich vu ngoai troi: ");
+        dichVuNT = sc.nextDouble();
         sc.nextLine();
     }
 
-    @Override
     public void xuat() {
-        System.out.println("Mã phòng: " + maPhong);
-        System.out.println("Đơn giá: " + donGia + " VND/Ngày");
-        System.out.println("Loại phòng: " + getLoaiPhong());
-        System.out.println("Số người: " + soNguoi);
-        System.out.println("Số giường: " + soGiuong);
-        System.out.println("Tình trạng: " + kiemTraTinhTrang());
-        System.out.println("Phí đưa đón: " + PhiDuaDon);
-        System.out.println("Phí dịch vụ ngoài trời: " + dichVuNT);
+        super.xuat();
+        System.out.println("Loai phong: " + getLoaiPhong() + "\tPhi dua don: " + phiDuaDon
+                + "\tPhi dich vu ngoai troi: " + dichVuNT);
     }
-
-    public double tinhGiamGia(int soNgay) {
-        if (soNgay >= 7) {
-            return donGia * soNgay * 0.1;
-        }
-        return 0;
-    }
-
 }
